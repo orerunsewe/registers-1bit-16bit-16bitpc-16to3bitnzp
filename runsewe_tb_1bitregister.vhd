2588690c1a6bc1ec05b17eb05509a40b972e654c
Ore <37173532+orerunsewe@users.noreply.github.com>
2018-11-02 13:37:12 -0400

    Update runsewe_tb_1bitregister.vhd

--- runsewe_tb_1bitregister.vhd
+++ runsewe_tb_1bitregister.vhd
@@ -48,9 +48,9 @@ END COMPONENT;
 
 FILE in_file : TEXT open read_mode is 	"runsewe_1bitregisterin.txt";   -- Inputs, RST, enr,enl
 FILE exo_file : TEXT open read_mode is 	"runsewe_1bitregisterout.txt";   -- Expected output (binary)
-FILE out_file : TEXT open  write_mode is  "AMOO_EVENPD_Book_dataout_dacus.txt";
-FILE xout_file : TEXT open  write_mode is "AMOO_EVENPD_Book_TestOut_dacus.txt";
-FILE hex_out_file : TEXT open  write_mode is "AMOO_EVENPD_Book_hex_out_dacus.txt";
+FILE out_file : TEXT open  write_mode is  "runsewe_dataout_dacus.txt";
+FILE xout_file : TEXT open  write_mode is "runsewe_TestOut_dacus.txt";
+FILE hex_out_file : TEXT open  write_mode is "runsewe_hex_out_dacus.txt";
 
 ---------------------------------------------------------------
 -- SIGNALS 
